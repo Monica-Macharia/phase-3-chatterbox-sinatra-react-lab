cb8398b2fff4d5121a906dfdd20d1d47337324e1
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-02 18:26:13 +0300

    crud, models, migration

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,31 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+ 
+    
+    
+    
+    post "/messages" do
+      news = Message.create(body: params[:body], username: params[:username])
+      news.to_json
+    end
+    
+    patch "/messages/:id" do
+      text = Message.find(params[:id])
+      text.update(body: params[:body])
+      text.to_json
+    end
+    get "/messages" do
+      texts = Message.all.order(:created_at)
+      texts.to_json
+    end
+    
+    delete "/messages/:id" do
+      texts = Message.find(params[:id])
+      texts.destroy
+      texts.to_json
+    end
+    
+
   
 end
